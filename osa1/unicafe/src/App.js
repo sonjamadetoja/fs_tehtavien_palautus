db989f67c76357260b2bd5c0fbb00654033a7182
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-04-05 20:46:00 +0300

    1.6: unicafe step1

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+const Header = ({header}) => <h1> {header} </h1>
+const Display = ({name, value}) => <div> {name} {value} </div>
+
+const Button = ({handleClick, text}) => {
+  return (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => {
+    console.log(newValue)
+    setGood(newValue)
+  }
+
+  const setToNeutral = newValue => {
+    setNeutral(newValue)
+  }
+
+  const setToBad = newValue => {
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <Header header='give feedback' />
+      <Button handleClick={() => setToGood(good+1)} text='good' />
+      <Button handleClick={() => setToNeutral(neutral+1)} text='neutral' />
+      <Button handleClick={() => setToBad(bad+1)} text='bad' />
+      <Header header='statistics' />
+      <Display name='good' value={good} />
+      <Display name='neutral' value={neutral} />
+      <Display name='bad' value={bad} />
+    </div>
+  )
+}
+
+export default App
