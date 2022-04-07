8cf3494877da1d97d0eca00ba8de9cc26cebf433
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-04-07 15:48:29 +0300

    1.9: unicafe step4

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -12,16 +12,20 @@ const Button = ({handleClick, text}) => {
 }
 
 const Statistics = ({good, bad, neutral}) => {
+  if (good+neutral+bad > 0) {
+    return (
+      <div>
+        <Display name='good' value={good} />
+        <Display name='neutral' value={neutral} />
+        <Display name='bad' value={bad} />
+        <Display name='all' value={good+neutral+bad} />
+        <Display name='average' value={(good-bad)/good+neutral+bad} />
+        <Display name='positive' value={good/(good+neutral+bad)} />
+      </div>
+    )
+  }
   return (
-    <div>
-      <Header header='statistics' />
-      <Display name='good' value={good} />
-      <Display name='neutral' value={neutral} />
-      <Display name='bad' value={bad} />
-      <Display name='all' value={good+neutral+bad} />
-      <Display name='average' value={(good-bad)/good+neutral+bad} />
-      <Display name='positive' value={good/(good+neutral+bad)} />
-    </div>
+    <div>No statistics given</div>
   )
 }
 
@@ -51,6 +55,7 @@ const App = () => {
       <Button handleClick={() => setToGood(good+1)} text='good' />
       <Button handleClick={() => setToNeutral(neutral+1)} text='neutral' />
       <Button handleClick={() => setToBad(bad+1)} text='bad' />
+      <Header header='statistics' />
       <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
