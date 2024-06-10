a55d69126727892e43dbe81f4fc53985a1bb4910
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-10 14:04:34 +0300

    2.2: kurssitiedot step7

--- osa2/courseinfo/src/App.js
+++ osa2/courseinfo/src/App.js
@@ -1,28 +1,30 @@
 const Course = ({course}) =>
     <div>
-    <Header course={course} />
+    <Header name={course.name} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
     </div>
 
-const Header = ({ course }) => <h1>{course.name}</h1>
+const Header = ({ name }) => <h1>{name}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+const Total = ({ parts }) => {
+  const total = parts.reduce((accumulator, part) => {return accumulator + part.exercises}, 0)
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
 
 const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
+  <p>{part.name} {part.exercises}</p>
 
 const Content = ({ parts }) => 
-  <div>
-    <ul>
-      {parts.map(part =>
-        <Part key={part.id}
-          part={part}
-        />
-        )}
-    </ul>
-  </div>
+    <div>
+        {parts.map(part =>
+          <Part key={part.id}
+            part={part}
+          />
+          )}
+    </div>
 
 const App = () => {
   const course = {
