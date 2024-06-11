f454711d9c9939fcc1d410382d662394393fd352
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-11 15:07:23 +0300

    2.9*: puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -8,10 +8,15 @@ const Person = ({name, number}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231234' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchCondition, setSearchCondition] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -19,6 +24,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchCondition(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -37,9 +46,15 @@ const App = () => {
     }
   }
 
+  const peopleToShow = persons.filter(p => p.name.toLowerCase().includes(searchCondition.trim().toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with 
+        <input value={searchCondition} onChange={handleSearchChange}/>
+      </div>
+      <h2>add new</h2>
       <form onSubmit={addPerson}>
         <div>nimi: <input value={newName} onChange={handleNameChange}/></div>
         <div>numero: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -47,7 +62,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => 
+        {peopleToShow.map(person => 
         <Person key={person.name} name={person.name} number={person.number} />)}
       </div>
     </div>
