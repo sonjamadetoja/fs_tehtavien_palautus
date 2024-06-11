b4fab1540d178aaefd3cda789b12056a85b35340
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-11 10:55:29 +0300

    2.6: puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Person = ({name}) => {
+  return (
+    <p>{name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+        <Person key={person.name} name={person.name} />)}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
