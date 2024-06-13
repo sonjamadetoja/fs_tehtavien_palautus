06baf9ec1e5ce2c6610f02e33beaf3b768744082
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-13 15:51:25 +0300

    2.13: puhelinluettelo step8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Contact from './components/Contact'
 import Filter from './components/Filter'
 import List from './components/List'
+import contactService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,10 +11,10 @@ const App = () => {
   const [searchCondition, setSearchCondition] = useState('')
 
   useEffect(() => {
-    axios
-    .get('http://localhost:3001/persons')
-    .then((response) => {
-      setPersons(response.data)
+    contactService
+    .getAll()
+    .then((initialPersons) => {
+      setPersons(initialPersons)
     })
   }, [])
 
@@ -40,10 +40,10 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      contactService
+      .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
         })
