bcd1d5a22734e8b0e8cadfba9b85c5f8974b6a9e
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-06-13 15:01:06 +0300

    2.12: puhelinluettelo step7

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -40,9 +40,13 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
