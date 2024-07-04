970fa21dd62a60ed587bef3ee6090224e8f77dc8
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-07-04 17:52:16 +0300

    4.2 blogilista, step2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = config.MONGODB_URI
+
+logger.info('connecting to', mongoUrl)
+
+mongoose.connect(mongoUrl)
+.then(() => {
+  logger.info('connected to MongoDB')
+})
+.catch((error) => {
+  logger.error('error connecting to MongoDB:', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
