c3a3320b91a6332f5df1882bb379bb6240144db2
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-07-07 21:49:19 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    return blogs
+    .map(blog => blog.likes)
+    .reduce((sum, blog) => sum+blog, 0)
+}
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+  dummy, totalLikes
+}
