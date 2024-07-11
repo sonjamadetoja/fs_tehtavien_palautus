5f9e22165089c1c3ff5843d5dfd1baaf006d1c57
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-07-11 18:19:25 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -7,7 +7,28 @@ const totalLikes = (blogs) => {
     .map(blog => blog.likes)
     .reduce((sum, blog) => sum+blog, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return undefined
+    }
+    let mostLikes = 0
+    let biggest;
+    for (let i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > mostLikes) {
+            mostLikes = blogs[i].likes
+            biggest = blogs[i]
+        }
+    }
+    const result = 
+    {
+        title: biggest.title,
+        author: biggest.author,
+        likes: biggest.likes
+    }
+    return result
+}
   
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
