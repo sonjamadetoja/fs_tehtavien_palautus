eef7bc8fc395f8379cf5d348db3199a28694e055
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-07 16:19:27 +0300

    4.18: blogilistan laajennus, step6 & 4.19: blogilistan laajennus, step7

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 
@@ -25,6 +26,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
