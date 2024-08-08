61d253c494104526a0c78b87292186308e444bc8
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-08 13:10:04 +0300

    4.23*: blogilistan laajennus, step11

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -34,97 +34,140 @@ test('blog identifier is called \'id\'', async () => {
   assert.deepStrictEqual(titles, newTitles)
 })
 
-test('a new blog can be added', async () => {
-  const newBlog = {
-    title: "Testi3",
-    author: "Mina3",
-    url: "www.blogi3.com",
-    likes: 300
-  }
+describe('blog creation and deletion when there is one user in database and logged in', () => {
+  let token;
+  beforeEach(async () => {
+    await User.deleteMany({})
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const password = 'sekret'
+    const passwordHash = await bcrypt.hash(password, 10)
+    const user = new User({ username: "root", passwordHash })
+    const userForToken = { username: "root", password }
 
-  const response = await api.get('/api/blogs')
-  const blogs = response.body.map(b => b.title)
+    await user.save()
 
-  assert.equal(response.body.length, helper.initialBlogs.length + 1)
-  assert(blogs.includes('Testi3'))
-})
+    const result = await api
+      .post('/api/login/')
+      .send(userForToken)
 
-test('if \'likes\' is empty it is set to zero', async () => {
-  const newBlog = {
-    title: "Testi3",
-    author: "Mina3",
-    url: "www.blogi3.com"
-  }
+    token = result.body.token
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  test('a new blog can be added', async () => {
+    const newBlog = {
+      title: "Testi3",
+      author: "Mina3",
+      url: "www.blogi3.com",
+      likes: 300
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const blogs = response.body.map(b => b.title)
+
+    assert.equal(response.body.length, helper.initialBlogs.length + 1)
+    assert(blogs.includes('Testi3'))
+  })
+
+  test('if \'likes\' is empty it is set to zero', async () => {
+    const newBlog = {
+      title: "Testi3",
+      author: "Mina3",
+      url: "www.blogi3.com"
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+      const response = await api.get('/api/blogs')
+      const blogs = response.body
+
+      let likes;
+
+      for (x in blogs) {
+        if (blogs[x].title == "Testi3") {
+          likes = blogs[x].likes
+        }
+      }
+
+      assert.equal(likes, 0)
+  })
+
+  test('if \'title\' is empty, respond with \'400 Bad Request\'', async () => {
+    const newBlog = {
+      author: "Mina3",
+      url: "www.blogi3.com",
+      likes: 300
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(400)
+
+      assert.strictEqual(response.status, 400)
+  })
+
+  test('if \'url\' is empty, respond with \'400 Bad Request\'', async () => {
+    const newBlog = {
+      title: "Testi3",
+      author: "Mina3",
+      likes: 300
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(400)
+
+      assert.strictEqual(response.status, 400)
+  })
+
+  test('deletion of a blog', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const user = await User.findOne({ username: "root" })
+
+    const blogToInsertAndDelete = {
+      title: "Testi3",
+      author: "Mina3",
+      url: "www.blogi3.com",
+      likes: 300,
+      user: user.id
+    }
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(blogToInsertAndDelete)
 
     const response = await api.get('/api/blogs')
     const blogs = response.body
+    const blogInserted = blogs.find(blog => blog.title === blogToInsertAndDelete.title)
 
-    let likes;
+    await api
+      .delete(`/api/blogs/${blogInserted.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(204)
 
-    for (x in blogs) {
-      if (blogs[x].title == "Testi3") {
-        likes = blogs[x].likes
-      }
-    }
+    const blogsAfterDeletion = await helper.blogsInDb()
+    const titles = blogsAfterDeletion.map(b => b.title)
 
-    assert.equal(likes, 0)
-})
+    assert.strictEqual(blogsAfterDeletion.length, blogsAtStart.length)
+    assert(!titles.includes(blogToInsertAndDelete.title))
 
-test('if \'title\' is empty, respond with \'400 Bad Request\'', async () => {
-  const newBlog = {
-    author: "Mina3",
-    url: "www.blogi3.com",
-    likes: 300
-  }
-
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-    assert.strictEqual(response.status, 400)
-})
-
-test('if \'url\' is empty, respond with \'400 Bad Request\'', async () => {
-  const newBlog = {
-    title: "Testi3",
-    author: "Mina3",
-    likes: 300
-  }
-
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-    assert.strictEqual(response.status, 400)
-})
-
-test('deletion of a blog', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-  const blogToDelete = blogsAtStart[0]
-
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
-
-  const blogsAfterDeletion = await helper.blogsInDb()
-  const titles = blogsAfterDeletion.map(b => b.title)
-
-  assert.strictEqual(blogsAfterDeletion.length, blogsAtStart.length - 1)
-  assert(!titles.includes(blogToDelete.title))
+  })
 
 })
 
