66191ef36b95950332ec6824bbd4eddc0ee7aa18
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-09 16:24:33 +0300

    FIX 5.1: add name & "logged in" to page; START 5.2: useEffect for fetching token from localstorage

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -16,10 +16,19 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedInBlogUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -31,13 +40,16 @@ const App = () => {
   if (user === null) {
     return (
       <div>
-        <LoginForm handleLogin={handleLogin} username={username} setUsername={setUsername} password={password} setPassword={setPassword} />
+        <LoginForm handleLogin={handleLogin} 
+        username={username} setUsername={setUsername} 
+        password={password} setPassword={setPassword} />
       </div>
     )
   } 
 
   return (
     <div>
+      {user.name} is logged in.
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
