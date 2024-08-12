a7adad540657a3baa8b463a08d89c9ab32323352
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-12 11:01:03 +0300

    5.3: blogilistan frontend, step3

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -9,6 +9,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -28,7 +31,10 @@ const App = () => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+
       window.localStorage.setItem('loggedInBlogUser', JSON.stringify(user))
+
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -41,6 +47,37 @@ const App = () => {
     setUser(null)
     window.localStorage.removeItem('loggedInBlogUser')
   }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+
+    try {
+      const returnedNote = await blogService.create(blogObject)
+      setBlogs(blogs.concat(returnedNote))
+      setNewTitle('')
+      setNewAuthor('')
+      setNewUrl('')
+    } catch (exception) {
+      console.log('exception', exception)
+    }
+  }
+
+  const blogForm = () => (
+    <div>
+      <form onSubmit={addBlog}>
+        title: <input type='text' value={newTitle} name='title' onChange={({ target }) => {setNewTitle(target.value)}} />
+        author: <input type='text' value={newAuthor} name='author' onChange={({ target }) => {setNewAuthor(target.value)}} />
+        url: <input type='text' value={newUrl} name='url' onChange={({ target }) => {setNewUrl(target.value)}} />
+        <button type='submit'>save</button>
+      </form>
+    </div>
+  )
 
   if (user === null) {
     return (
@@ -55,6 +92,10 @@ const App = () => {
   return (
     <div>
       {user.name} is logged in. <button onClick={handleLogout}>logout</button>
+      <h2>create new</h2>
+        <div>
+          {blogForm()}
+        </div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
