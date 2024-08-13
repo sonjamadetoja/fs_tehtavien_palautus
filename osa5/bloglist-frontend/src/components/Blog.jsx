4707a43daa0534eb2bca59f572c8faee251e907d
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-13 13:56:41 +0300

    5.7 blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 3,
+    border: 'solid',
+    borderWidth: 2,
+    marginBottom: 5
+  }
+  const [detailVisibility, setDetailVisibility] = useState(false)
+
+  const hideWhenDetailsVisible = { display: detailVisibility ? 'none' : '' }
+  const showWhenDetailsVisible = { display: detailVisibility ? '' : 'none'}
+
+  const toggleDetailVisibility = () => {
+    setDetailVisibility(!detailVisibility)
+}
+  return (
+  <div style={blogStyle}>
     {blog.title} {blog.author}
+
+    <button onClick={toggleDetailVisibility} style={hideWhenDetailsVisible}>view</button>
+    <button onClick={toggleDetailVisibility} style={showWhenDetailsVisible} >hide</button>
+
+    <div style={showWhenDetailsVisible}>
+      {blog.url} <br />
+      likes {blog.likes} <button>like</button> <br />
+      {blog.user.name}
+    </div>
   </div>  
-)
+)}
 
 export default Blog
