96cb942eead20c3595694e529042703fca07e38e
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-16 11:40:28 +0300

    remove unnecessary import

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { expect } from 'vitest'
 
 const testBlog = {
   title: 'Test Blog Name',
