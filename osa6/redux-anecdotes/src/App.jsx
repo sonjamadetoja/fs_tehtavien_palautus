0d22e48ace3366ebd14012c4466b2dbde7c1c287
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-23 14:42:12 +0300

    6.16 anekdootit ja backend, step3

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,17 +1,15 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import anecdoteService from './services/anecdoteService'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService
-     .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
 
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import anecdoteService from "../services/anecdoteService"
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -22,4 +23,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, increaseVotes, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
