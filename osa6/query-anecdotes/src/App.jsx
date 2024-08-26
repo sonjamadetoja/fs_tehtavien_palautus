43087fed644d899db10cb16d4a1e9b24e816c7e0
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-26 13:34:43 +0300

    Tehtävä 6.20

---
+++ osa6/query-anecdotes/src/App.jsx
@@ -0,0 +1,56 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { getAnecdotes } from './requests/requests'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isPending) {
+    console.log('loading...')
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    console.log('anecdote service is not available due to problems in server')
+    return <div>anecdote service is not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
